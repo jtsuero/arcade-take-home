67f84f2f701d9c160ed4a1bc944825f265573079
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2025-09-11 11:12:54 -0700

    Cards are now fetched directly from db

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,32 @@
 import Button from "./components/Button";
 import { Card } from "@/lib/types";
 import { CARD_COLORS } from "@/lib/types";
-
-const dummyCards: Card[] = [
-	{
-		id: 1,
-		title: "Card 1",
-		description: "Description 1",
-		fillColor: "orange",
-		createdAt: "2025-01-01",
-	},
-	{
-		id: 2,
-		title: "Card 2",
-		description: "Description 2",
-		fillColor: "teal",
-		createdAt: "2025-01-01",
-	},
-];
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	const [cards, setCards] = useState<Card[]>([]);
+
+	useEffect(() => {
+		fetchCards();
+	}, []);
+
+	const fetchCards = async () => {
+		const res = await fetch("/api/cards");
+		const data = await res.json();
+		setCards(data);
+	};
+
 	return (
 		<div className='flex flex-col items-center justify-center h-screen'>
-			<div>
-				<div>Card Manager</div>
+			<div className='text-3xl font-bold text-gray-900'>Card Manager</div>
+			<div className='flex justify-center mb-8'>
 				<Button variant='primary' route='/cards/new' text={"Add New Card"}>
 					Add New Card
 				</Button>
 			</div>
 			<div>
 				<div>Cards</div>
-				{dummyCards.map((card) => (
+				{cards.map((card) => (
 					<div
 						key={card.id}
 						className='flex flex-col gap-2 p-4 rounded-lg'
